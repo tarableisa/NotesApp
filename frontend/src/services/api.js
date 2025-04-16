ebb874f9740842d78fc60d3a6bf8f43710428292
tarableisa <tarableisa@gmail.com>
2025-04-16 20:58:26 +0700

    Mengubah api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,13 +8,13 @@ const API_URL = `${ BASE_URL}/notes`;
 export const getNotes = async () => await axios.get(API_URL);
 
 // Get note by ID
-export const getNote = async (id) => await axios.get(`${ BASE_URL}/${id}`);
+export const getNote = async (id) => await axios.get(`${ API_URL}/${id}`);
 
 // Create new note
 export const createNote = async (note) => await axios.post(API_URL, note);
 
 // Update note
-export const updateNote = async (id, note) => await axios.put(`${ BASE_URL}/${id}`, note);
+export const updateNote = async (id, note) => await axios.put(`${ API_URL}/${id}`, note);
 
 // Delete note
-export const deleteNote = async (id) => await axios.delete(`${BASE_URL}/${id}`);
+export const deleteNote = async (id) => await axios.delete(`${API_URL}/${id}`);
