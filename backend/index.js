a9fcdc5e8275697e03dcd927e178c8fef693ffa4
tarableisa <tarableisa@gmail.com>
2025-05-14 00:07:20 +0700

    ayo

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Konfigurasi CORS agar mengizinkan domain frontend terdeploy
 const corsOptions = {
