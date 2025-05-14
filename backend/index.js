694944256c38330f44d6f48d6dbe496c8cbab672
tarableisa <tarableisa@gmail.com>
2025-05-14 22:23:05 +0700

    bisa

--- backend/index.js
+++ backend/index.js
@@ -44,4 +44,6 @@ app.use(NotesRoute);
   }
 })();
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
